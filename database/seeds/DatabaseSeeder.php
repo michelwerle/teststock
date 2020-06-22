28b68b0096717fc28a306f2d8ccf2339dfb0216d
Michel <michel.werle@geauto.fr>
2020-06-22 16:20:53 +0200

    Readme Données

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,19 +19,53 @@ class DatabaseSeeder extends Seeder
                     'password' => bcrypt('azerty'),
                 ]
             );
-            App\Produit::create(
-                [
+            App\Produit::insert(
+                [[
+                    'id' => 1,
                     'nom' => 'Coca-Cola',
                     'description' => 'Formidable canette rouge',
                     'prix' => 0.95,
                     'quantite' => 6
                 ],
                 [
+                    'id' => 2,
                     'nom' => 'Xbox One X',
                     'description' => 'Console de jeu ultra-puissante',
                     'prix' => 399,
                     'quantite' => 2
-                ]
+                ]]
+            );
+            App\Mouvement::insert([
+                [
+                    'produit_id' => 1,
+                    'created_at' => '2020-06-19 10:00:00',
+                    'old' => '{"nom":"Coca-Cola","description":"Formidable canette rouge","prix":0.95,"quantite":6}',
+                    'new' => '{"nom":"Coca-Cola","description":"Formidable canette rouge 2","prix":0.8,"quantite":9}'
+                ],
+                [
+                    'produit_id' => 1,
+                    'created_at' => '2020-06-20 15:00:00',
+                    'old' => '{"prix":0.8,"quantite":9}',
+                    'new' => '{"prix":1,"quantite":12}'
+                ],
+                [
+                    'produit_id' => 1,
+                    'created_at' => '2020-06-21 10:00:00',
+                    'old' => '{"prix":1,"quantite":12}',
+                    'new' => '{"prix":0.9,"quantite":10}'
+                ],
+                [
+                    'produit_id' => 2,
+                    'created_at' => '2020-06-19 10:00:00',
+                    'old' => '{"prix":399,"quantite":2}',
+                    'new' => '{"prix":299,"quantite":5}'
+                ],
+                [
+                    'produit_id' => 2,
+                    'created_at' => '2020-06-12 10:00:00',
+                    'old' => '{"prix":299,"quantite":5}',
+                    'new' => '{"prix":299,"quantite":4}'
+                ]]
             );
     }
 }
